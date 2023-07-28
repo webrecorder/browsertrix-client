523418cf144b60412e04bc6cd63a680b8e29ec42
Anish Lakhwara <anish+git@lakhwara.com>
2023-07-28 14:00:41 -0700

    fix: tessas changes

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 const fs = require("fs");
 const { Readable } = require("node:stream");
 
-class APIClient
-{
-  static async login(url, username, password) {
-    const loginUrl = url + "/api/auth/jwt/login";
+export class APIClient {
+  static async login(
+    url = "https://dev.browsertrix.cloud",
+    username = "dev@webrecorder.net",
+    password,
+  ) {
+    const marhsalledUrl = url.endsWith("/") ? url.slice(0, -1) : url;
+    const loginUrl = marhsalledUrl + "/api/auth/jwt/login";
 
     const form = new FormData();
     form.append("username", username);
     form.append("password", password);
 
-    const res = await fetch(loginUrl, {method: "POST", body: form});
+    const res = await fetch(loginUrl, { method: "POST", body: form });
     const auth = await res.json();
-    const {token_type, access_token} = auth;
+    const { token_type, access_token } = auth;
 
     const authToken = token_type + " " + access_token;
-    return new APIClient(url, authToken);
+    return new APIClient(marhsalledUrl, authToken);
   }
 
   constructor(url, auth) {
-    this.url = url;
+    this.url = url.endsWith("/") ? url.slice(0, -1) : url;
     this.auth = auth;
   }
 
-  async fetchAPI(endpoint, method="GET", body=null) {
-    const headers = {"Authorization": this.auth, "Content-Type": "application/json"};
-    const resp = await fetch(this.url + endpoint, {headers, method, body, duplex: "half"});
+  async fetchAPI(endpoint, method = "GET", body = null) {
+    const headers = {
+      Authorization: this.auth,
+      "Content-Type": "application/json",
+    };
+    const resp = await fetch(this.url + endpoint, {
+      headers,
+      method,
+      body,
+      duplex: "half",
+    });
     return await resp.json();
   }
 
-  async getOrg(name="") {
+  async getOrg(name = "") {
     const json = await this.fetchAPI("/api/users/me-with-orgs");
     const { orgs } = json;
     if (!orgs || !orgs.length) {
@@ -46,50 +58,61 @@ class APIClient
     return orgs[0].id;
   }
 
-	async getCrawlerWorkflows(org) {
-		const res = await this.fetchAPI(`/api/${org}/crawlconfigs`)
-		return res;
-	}
+  async getCrawlerWorkflows(org) {
+    return await this.fetchAPI(`/api/${org}/crawlconfigs`);
+  }
 
-	async getCrawlerWorkflow(org, cid="") {
-		const res = await this.fetchAPI(`/api/${org}/crawlconfigs/${cid}`)
-		return res;
-	}
+  async getCrawlerWorkflow(org, cid = "") {
+    return await this.fetchAPI(`/api/${org}/crawlconfigs/${cid}`);
+  }
 
-	async updateCrawlerWorkflow(org, cid="", config) {
-		if (cid && config) {
-			const res = await this.fetchAPI(`/api/${org}/crawlconfigs/${cid}`, method="PATCH", body=config)
-			return res;
-		}
-		console.log("You must provide a Crawler ID and a Crawler Workflow to update")
-	}
+  async updateCrawlerWorkflow(org, cid = "", config) {
+    if (cid && config) {
+      return await this.fetchAPI(
+        `/api/${org}/crawlconfigs/${cid}`,
+        (method = "PATCH"),
+        (body = config),
+      );
+    }
+    console.log("You must provide a Crawler Workflow ID to update");
+  }
 
-	async deleteCrawlerWorkflow(org, cid="") {
-		if (cid) {
-			const res = await this.fetchAPI(`/api/${org}/crawlconfigs/${cid}`, method="DELETE")
-			return res;
-		}
-		console.log("You must provide a Crawler ID")
-	}
+  async deleteCrawlerWorkflow(org, cid = "") {
+    if (cid) {
+      return await this.fetchAPI(
+        `/api/${org}/crawlconfigs/${cid}`,
+        (method = "DELETE"),
+      );
+    }
+    console.log("You must provide a Crawler ID");
+  }
 
-	async addCrawlerWorkflow(org, config) {
-		if (config) {
-			const res = await this.fetchAPI(`/api/${org}/crawlconfigs/`, method="POST", body=config)
-			return res;
-		}
-		console.log("You must provide a Crawler Workflow")
-	}
+  async addCrawlerWorkflow(org, config) {
+    if (config) {
+      return await this.fetchAPI(
+        `/api/${org}/crawlconfigs/`,
+        (method = "POST"),
+        (body = config),
+      );
+    }
+    console.log("You must provide a Crawler Workflow");
+  }
 
-	async runCrawl(org, cid="") {
-		if (cid) {
-			const res = await this.fetchAPI(`/api/${org}/crawlconfigs/${cid}/run`, method="POST")
-			return res;
-		}
-		console.log("You must provide a Crawler ID")
-	}
+  async runCrawl(org, cid = "") {
+    if (cid) {
+      return await this.fetchAPI(
+        `/api/${org}/crawlconfigs/${cid}/run`,
+        (method = "POST"),
+      );
+    }
+    console.log("You must provide a Crawler ID");
+  }
 
   async upload(org, data) {
-    const res = await this.fetchAPI(`/api/orgs/${org}/uploads/stream?name=` + process.argv[2], "PUT", data);
-    return res;
+    return await this.fetchAPI(
+      `/api/orgs/${org}/uploads/stream?name=` + process.argv[2],
+      "PUT",
+      data,
+    );
   }
 }
